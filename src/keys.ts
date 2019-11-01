7e3821d3e44a64256b54d2447af45a7d1f2e7f88
Craig Nishina <craig.nishina@gmail.com>
2019-10-31 22:39:18 -0700

    chore(keys): use selenium-webdriver dependency for keys

--- src/keys.ts
+++ src/keys.ts
@@ -1,12 +1,11 @@
 /**
- * Keys and key combinations commonly used in Pantheon tests. Most of these
- * are direct exports from protractor.Keys, and in most cases users should
+ * Keys and key combinations commonly used in tests. Most of these
+ * are direct exports from webdriver keys, and in most cases users should
  * grab keys directly from there instead of adding new exports to this file.
  */
-import {protractor} from 'protractor';
+import {Key} from 'selenium-webdriver';
 
-export const CLEAR = protractor.Key.CONTROL + protractor.Key.END +
-    protractor.Key.SHIFT + protractor.Key.HOME +
-    protractor.Key.NULL /* Release modifier keys. */ + protractor.Key.DELETE;
-export const ENTER = protractor.Key.ENTER;
-export const TAB = protractor.Key.TAB;
+export const CLEAR = Key.CONTROL + Key.END + Key.SHIFT + Key.HOME +
+    Key.NULL /* Release modifier keys. */ + Key.DELETE;
+export const ENTER = Key.ENTER;
+export const TAB = Key.TAB;
